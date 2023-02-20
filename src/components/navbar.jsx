175257fe309b76d748232781270e83370dd7f544
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-02-20 12:09:34 +0530

    made responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 export default function Navbar() {
     return (
-        <div className="nav">
-            <h4 className="logo-text">abNORMAL</h4>
-            <ul>
-                <li>
-                    <a href="#home">Home</a>
-                </li>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a href="mailto:piyushbhagwat5@gmail.com?subject=Work Request!">lets Work</a>
-                </li>
-            </ul>
-        </div>
+        <>
+            <div className="nav ">
+                <h4 className="logo-text">abNORMAL</h4>
+                <ul>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="mailto:piyushbhagwat5@gmail.com?subject=Work Request!">
+                            lets Work
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
